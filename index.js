f06d9bcc5ff9d4927d22f758ca948314aeb0ff06
TanishqBagde <tanishqbagde1618@gmail.com>
2024-05-30 23:25:55 +0530

    cors updates

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'https://appletvgpt.netlify.app',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true
