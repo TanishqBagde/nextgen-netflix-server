29ab1d9107a106059f13f468da2f64faabe64503
TanishqBagde <tanishqbagde1618@gmail.com>
2024-05-30 23:37:40 +0530

    updates

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     allowedHeaders: 'Content-Type,Authorization',
     credentials: false
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/v1/user", userRoutes);
 
