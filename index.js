0fd9573973091029354ca473ec0600f657fde37b
TanishqBagde <tanishqbagde1618@gmail.com>
2024-05-30 23:21:28 +0530

    updated cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const dataBaseConnection = require ("./utils/dataBase.js");
+const dataBaseConnection = require("./utils/dataBase.js");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/userRoutes.js");
 const cors = require("cors");
@@ -8,24 +8,24 @@ const cors = require("cors");
 dataBaseConnection();
 
 dotenv.config({
-    path:".env"
-})
+    path: ".env"
+});
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-
 const corsOptions = {
-  origin: 'https://appletvgpt.netlify.app',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-  credentials: true 
-}
+    origin: 'https://appletvgpt.netlify.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+};
 app.use(cors(corsOptions));
-app.use("/api/v1/user",userRoutes);
+
+app.use("/api/v1/user", userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running at PORT ${process.env.PORT}`);
