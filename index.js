7fdc7768197dff23246d3c6d8eed16516c4a132d
TanishqBagde <tanishqbagde1618@gmail.com>
2024-05-31 16:08:14 +0530

    updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 dataBaseConnection();
 console.log("test");
 
-const PORT = process.env.PORT || 3000; // Default to port 3000 if PORT environment variable is not set
+const PORT = process.env.PORT || 3500; // Default to port 3500 if PORT environment variable is not set
 const ALLOWED_ORIGIN = 'https://appletvgpt.netlify.app'; // Define the allowed origin
 
 const app = express();
@@ -16,27 +16,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-const corsOptions = {
-    origin: ALLOWED_ORIGIN,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
 // Use CORS middleware with specified options
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: ALLOWED_ORIGIN,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Handle preflight requests
-app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(204);
-});
+app.options('*', cors({
+  origin: ALLOWED_ORIGIN,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use("/api/v1/user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running at PORT ${PORT}`);
+  console.log(`Server running at PORT ${PORT}`);
 });
